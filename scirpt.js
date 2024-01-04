1c02b08725f5024be457dd3080ac0ce4dd7b0e3b
GnuWorm32 <stevanglavaskii@gmail.com>
2024-01-04 19:48:45 +0100

    Middle of Tutors

--- scirpt.js
+++ scirpt.js
@@ -10,25 +10,20 @@ menuBtn.onclick = () =>{
 
 
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
   direction: 'horizontal',
   loop: true,
-  // If we need pagination
-  
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-
+  slidesPerView: 3,
+  pagination: {
+    el: '.swiper-pagination',
+  }, 
   autoplay: {
-    delay:4000,
+    delay:7000,
   },
 });
+
+
+
+
+
+
 
